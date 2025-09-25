32086b86a45c66c8aeec222674b72c2307d0304c
Dandi Fonna Saputra <dandifonnasaputra@gmail.com>
2025-09-25 22:04:06 +0700

    refactor: Restructure UI and add session manager
    - refactor: Restructure UI for scan environment icon
    - feat: Add new Session Manager script to handle AR session state.

---
+++ Assets/Scripts/Utiils/SessionManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.XR.ARFoundation;
+
+public class SessionManager : MonoBehaviour
+{
+   [SerializeField] private GameObject uiScanningPanel;
+   [SerializeField] private TextMeshProUGUI textInfo;
+   [SerializeField] private int requiredPlaneCount = 2;
+
+   private ARPlaneManager arPlaneManager;
+
+   void Awake()
+   {
+      arPlaneManager = FindAnyObjectByType<ARPlaneManager>();
+   }
+
+   void OnEnable()
+   {
+      arPlaneManager.trackablesChanged.AddListener(OnPlanesChanged);
+   }
+
+   void OnDisable()
+   {
+      arPlaneManager.trackablesChanged.RemoveListener(OnPlanesChanged);
+   }
+
+   private void OnPlanesChanged(ARTrackablesChangedEventArgs<ARPlane> args)
+   {
+      HandleUiInfoAr();
+   }
+
+   private void HandleUiInfoAr()
+   {
+      if (arPlaneManager.trackables.count > requiredPlaneCount)
+      {
+         uiScanningPanel.SetActive(false);
+         textInfo.gameObject.SetActive(false);
+      }
+      else
+      {
+         uiScanningPanel.SetActive(true);
+         textInfo.gameObject.SetActive(true);
+         textInfo.text = "Arahkan ponsel perlahan untuk mendeteksi permukaan";
+      }
+   }
+}
