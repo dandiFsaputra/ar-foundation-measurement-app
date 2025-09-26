07b0cdfe468af434e8c7dc37713eda13de8f526f
Dandi Fonna Saputra <dandifonnasaputra@gmail.com>
2025-09-26 15:45:37 +0700

    feature: camera speed

--- Assets/Scripts/Utiils/SessionManager.cs
+++ Assets/Scripts/Utiils/SessionManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.XR.ARFoundation;
+using Unity.Mathematics;
 
 public class SessionManager : MonoBehaviour
 {
@@ -8,11 +9,18 @@ public class SessionManager : MonoBehaviour
    [SerializeField] private GameObject uiScanningPanel;
    [SerializeField] private TextMeshProUGUI textInfo;
    [SerializeField] private int requiredPlaneCount;
+
+   [Header("Camera Speed Settings")]
+   [SerializeField] private float speedThreshold = 1.5f;
+   [SerializeField] private float rotationThreshold = 100f;
+   [SerializeField] private float showDuration = 1.5f;
+
    public bool IsReadyToPlace { get; set; }
-
-
-
    public ARPlaneManager ArPlaneManager { get; set; }
+
+   private Vector3 lasPosition;
+   private quaternion lastRotation;
+   private float timer;
 
    void Awake()
    {
@@ -23,6 +31,12 @@ public class SessionManager : MonoBehaviour
    void Start()
    {
       HandleUiInfoAr();
+
+      if (Camera.main != null)
+      {
+         lasPosition = Camera.main.transform.position;
+         lastRotation = Camera.main.transform.rotation;
+      }
    }
 
    void OnEnable()
@@ -38,6 +52,34 @@ public class SessionManager : MonoBehaviour
    private void OnPlanesChanged(ARTrackablesChangedEventArgs<ARPlane> args)
    {
       HandleUiInfoAr();
+   }
+
+   void Update()
+   {
+      if (Camera.main == null) return;
+
+      Transform cam = Camera.main.transform;
+      float speed = (cam.position - lasPosition).magnitude / Time.deltaTime;
+
+      float angle = Quaternion.Angle(cam.rotation, lastRotation);
+      float angularSpeed = angle / Time.deltaTime;
+
+      if (speed > speedThreshold || angularSpeed > rotationThreshold)
+      {
+         textInfo.text = "Pergerakan kamera terlalu cepat, coba gerakan perlahan";
+         timer = showDuration;
+      }
+
+      if (timer > 0)
+      {
+         timer -= Time.deltaTime;
+         if (timer <= 0)
+         {
+            HandleUiInfoAr();
+         }
+      }
+      lasPosition = cam.position;
+      lastRotation = cam.rotation;
    }
 
    private void HandleUiInfoAr()
